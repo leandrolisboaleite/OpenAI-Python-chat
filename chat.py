56f93da6e7a749e37e50c4466e1a8a5090630e4a
uol13 <leandrolisboaleite@gmail.com>
2022-12-06 21:36:44 -0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,13 +1,10 @@
 import os
 import openai
 
-# Set the API key for OpenAI
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Prompt the user for a question
 question = input("Please enter a question for me to answer: ")
 
-# Make a request to OpenAI's GPT-3 model to generate a response to the question
 response = openai.Completion.create(
     engine="text-davinci-002",
     prompt=question,
@@ -18,5 +15,4 @@ response = openai.Completion.create(
     presence_penalty=0
 )
 
-# Print the response
 print(response["choices"][0]["text"])
